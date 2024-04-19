1d9fd0abc626b5d232e485bf0303060fb059e1e6
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-19 11:27:57 +0200

    Project dependency set up

---
+++ src/main/java/com/vincenzo/d5w3u5/GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package com.vincenzo.d5w3u5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiApplication.class, args);
+	}
+
+}
